8f123ec93cef937485fe8eac6a1882660ef05f72
DESKTOP-NK33SKA\林鵘鮿 <gen199999@gmail.com>
2018-07-09 21:47:49 +0800

    把網站上教的做過一遍了，晚點再想要特效怎麼加比較好

---
+++ roll a ball/roll a ball/Assets/scripts/player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+
+    public float speed;
+    public Text counttext;
+    public Text wintext;
+
+    private Rigidbody rb;
+    private int count;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        wintext.text = "";
+	}
+	
+	// Called before performing any physics calculations
+	void FixedUpdate ()
+    {
+
+        //設移動變數
+        float mh = Input.GetAxis("Horizontal");
+        float mv = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(mh, 0.0f, mv);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("point"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("point"))
+        {
+            other.gameObject.SetActive(false);
+            count = count +  1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        counttext.text = "Count:" + count.ToString();
+        if (count >= 25)
+        {
+            wintext.text = "Congratulations";
+        }
+    }
+}
